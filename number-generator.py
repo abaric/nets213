7a473517cb50f30f5ecd5fcd76dca4376f9fa0dc
ameliagoodman <ameliago@sas.upenn.edu>
2016-02-23 21:48:03 -0500

    fixed to print 3 per line

--- number-generator.py
+++ number-generator.py
@@ -4,13 +4,13 @@ import math
 # Code to generate 90 random numbers split by line
 
 for i in range(0, 90):
-	x1 = random.random()
+	x1 = random.random() * 1000
 	y1 = math.floor(x1)
-	x2 = random.random()
+	x2 = random.random() * 1000
 	y2 = math.floor(x2)
-	x3 = random.random()
+	x3 = random.random() * 1000
 	y3 = math.floor(x3)
-	while (y1 == y2 || y2 == y3):
+	while (y1 == y2) | (y2 == y3):
 		x1 = random.random()
 		y1 = math.floor(x1)
 		x2 = random.random()
@@ -18,6 +18,4 @@ for i in range(0, 90):
 		x3 = random.random()
 		y3 = math.floor(x3)
 	
-	print y1
-	print y2
-	print y3
+	print '%d%s%d%s%d' %(y1,", ",y2,", ", y3)
